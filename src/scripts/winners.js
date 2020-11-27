a14e47b52a844d06d76a02e8b982c409a0923cef
Surtaev Nikita <supergodask@gmail.com>
2020-11-27 17:09:29 +0300

    add winners second section

--- src/scripts/winners.js
+++ src/scripts/winners.js
@@ -4,13 +4,42 @@ Swiper.use([Navigation]);
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	const swiper = new Swiper('.winners-slider', {
-		loop: true,
-		spaceBetween: 30,
-		navigation: {
-			prevEl: '.winners-arrow.winners-arrow_role_prev',
-			nextEl: '.winners-arrow.winners-arrow_role_next'
-		}
-	})
+	const winnersSlider = document.querySelector('.winners-slider');
+
+	if (winnersSlider) {
+		new Swiper('.winners-slider', {
+			loop: true,
+			spaceBetween: 30,
+			navigation: {
+				prevEl: '.winners-arrow.winners-arrow_role_prev',
+				nextEl: '.winners-arrow.winners-arrow_role_next'
+			}
+		})
+	}
+
+	const prizes = document.querySelector('.winners-list');
+
+	if (prizes) {
+		new Swiper('.winners-list', {
+			slidesPerView: 1,
+			spaceBetween: 45,
+			navigation: {
+				prevEl: '.winners-arrow-second_role_prev',
+				nextEl: '.winners-arrow-second_role_next'
+			},
+			breakpoints: {
+				1400: {
+					slidesPerView: 4
+				},
+				900: {
+					slidesPerView: 3
+				},
+				640: {
+					slidesPerView: 2
+				}
+			}
+		})
+	}
+
 
 });
