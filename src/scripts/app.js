3ff03295d85fa64d5159bc6957c2e7165478e86f
Surtaev Nikita <supergodask@gmail.com>
2020-11-27 13:57:08 +0300

    add feed section

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,2 @@
 import './winners';
+import './feed'
---
+++ src/scripts/feed.js
@@ -0,0 +1,31 @@
+import Swiper, {Navigation} from "swiper";
+
+Swiper.use([Navigation]);
+
+document.addEventListener('DOMContentLoaded', () => {
+	new Swiper('.feed-slider', {
+		slidesPerView: 1,
+		slidesPerColumn: 1,
+		slidesPerColumnFill: 'column',
+		spaceBetween: 20,
+		navigation: {
+			prevEl: '.feed-arrow.feed-arrow_role_prev',
+			nextEl: '.feed-arrow.feed-arrow_role_next'
+		},
+		breakpoints: {
+			1024: {
+				spaceBetween: 24
+			},
+			900: {
+				slidesPerColumn: 2,
+				slidesPerView: 4
+			},
+			640: {
+				slidesPerView: 3
+			},
+			480: {
+				slidesPerView: 2,
+			}
+		}
+	})
+});
